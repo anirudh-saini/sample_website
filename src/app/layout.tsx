bba8e2baeca8203a721cce1d2285ad80027d74ad
Anirudh Saini <anirudh.saini852@gmail.com>
2024-03-01 17:32:55 +0530

    noindexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
     title: "Sample Websites",
     description: "Sample websites build my mennr",
+    robots: {
+        index: false,
+        follow: false,
+        nocache: true,
+        googleBot: {
+            index: false,
+            follow: false,
+            noimageindex: true,
+            "max-video-preview": -1,
+            "max-image-preview": "large",
+            "max-snippet": -1,
+        },
+    },
 };
 
 export default function RootLayout({
