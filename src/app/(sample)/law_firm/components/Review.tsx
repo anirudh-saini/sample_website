68266fa758d870f5d97ccdec2c9495c000ee7b2e
Anirudh Saini <anirudh.saini852@gmail.com>
2024-03-06 12:01:12 +0530

    LAW PAGE

--- src/app/(sample)/law_firm/components/Review.tsx
+++ src/app/(sample)/law_firm/components/Review.tsx
@@ -1,32 +1,62 @@
-import React from "react";
+"use client";
+import React, { useState, useRef, useEffect } from "react";
 import style from "./review.module.scss";
+const data = [
+    {
+        img: "/law_firm/review1.png",
+        text: "Exceptional craftsmanship! The construction team delivered our project with precision and professionalism. From start to finish, they exceeded our expectations. Highly recommend for quality work and attention to detail.",
+        name: "John",
+    },
+    {
+        img: "/law_firm/review2.png",
+        text: "Incredible experience working with this construction company! The team's expertise and dedication were evident in every phase of our project. Transparent communication, on-time delivery, and top-notch results. Our dream space has become a reality!",
+        name: "Rachel",
+    },
+    {
+        img: "/law_firm/review3.png",
+        text: "Choosing this construction company was the best decision for our home renovation. Courteous and skilled professionals, they turned our vision into a stunning reality. The quality of work and commitment to customer satisfaction were outstanding.",
+        name: "Sudhama",
+    },
+    {
+        img: "/law_firm/review4.png",
+        text: "Outstanding service from the initial consultation to project completion. The construction team demonstrated a high level of expertise and worked efficiently to bring our ideas to life. We are thrilled with the results and grateful for their exceptional work ethic.",
+        name: "Rachel",
+    },
+    {
+        img: "/law_firm/review1.png",
+        text: "Exceptional craftsmanship! The construction team delivered our project with precision and professionalism. From start to finish, they exceeded our expectations. Highly recommend for quality work and attention to detail.",
+        name: "John",
+    },
+];
 export default function Review() {
-	return (
-		<div className={style.review}>
-			<div className={style.item}>
-				<div className={style.item1}>
-					<div>
-						<img src="./law_firm/review1.png" alt="" />
-					</div>
-					<p>
-						Exceptional craftsmanship! The construction team
-						delivered our project with precision and
-						professionalism. From start to finish, they exceeded our
-						expectations. Highly recommend for quality work and
-						attention to detail.
-					</p>
-					<h4>John</h4>
-				</div>
+    const parentRef = useRef<HTMLDivElement | null>(null);
+    const handlePrevClick = () => {
+        if (!parentRef.current) return;
+        parentRef.current.scrollLeft -= 250;
+    };
 
-				{/* <div></div>
-                <div></div>
-                <div></div>
-                <div></div> */}
-			</div>
-			<div className={style.background}>
-				<div className={style.bg1}></div>
-				<div className={style.bg2}></div>
-			</div>
-		</div>
-	);
+    const handleNextClick = () => {
+        if (!parentRef.current) return;
+        parentRef.current.scrollLeft += 250;
+    };
+    const items = data.map((item) => {
+        return (
+            <div className={style.item1}>
+                <div>
+                    <img src={item.img} alt="" />
+                </div>
+                <p>{item.text}</p>
+                <h4>{item.name}</h4>
+            </div>
+        );
+    });
+    return (
+        <div className={style.review}>
+            <button onClick={handlePrevClick}> &#11164;</button>
+            <div className={style.item} ref={parentRef}>
+                {items}
+            </div>
+            <button onClick={handleNextClick}>&#11166; </button>
+        </div>
+    );
 }
--- src/app/(sample)/law_firm/components/Team.tsx
+++ src/app/(sample)/law_firm/components/Team.tsx
@@ -2,35 +2,37 @@ import React from "react";
 import style from "./team.module.scss";
 export default function team() {
     return (
-        <div className={style.team}>
-            <div className={style.head}>
-                <h3>Meet Our Legal Team</h3>
-                <p>
-                    Empowering justice with a dynamic team of legal experts. Our
-                    law firm combines passion and expertise to deliver
-                    unparalleled legal solutions tailored to your needs.
-                </p>
-            </div>
-            <div className={style.img}>
-                <div>
-                    <img src="./law_firm/team1.png" alt="" />
-                    <div>
-                        <h5>Williams </h5>
-                        <p>Founding Partner</p>
-                    </div>
+        <div className={style.overflow}>
+            <div className={style.team}>
+                <div className={style.head}>
+                    <h3>Meet Our Legal Team</h3>
+                    <p>
+                        Empowering justice with a dynamic team of legal experts.
+                        Our law firm combines passion and expertise to deliver
+                        unparalleled legal solutions tailored to your needs.
+                    </p>
                 </div>
-                <div>
-                    <img src="./law_firm/team2.png" alt="" />
+                <div className={style.img}>
                     <div>
-                        <h5> Tran</h5>
-                        <p>Founding Partner</p>
+                        <img src="./law_firm/team1.png" alt="" />
+                        <div>
+                            <h5>Williams </h5>
+                            <p>Founding Partner</p>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <img src="./law_firm/team3.png" alt="" />
                     <div>
-                        <h5>Wong</h5>
-                        <p>Founding Partner</p>
+                        <img src="./law_firm/team2.png" alt="" />
+                        <div>
+                            <h5> Tran</h5>
+                            <p>Founding Partner</p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="./law_firm/team3.png" alt="" />
+                        <div>
+                            <h5>Wong</h5>
+                            <p>Founding Partner</p>
+                        </div>
                     </div>
                 </div>
             </div>
