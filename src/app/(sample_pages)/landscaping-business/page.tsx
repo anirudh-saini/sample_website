206f293ef8b5e3d301121ed1b85a0ebf87835f71
rohan <rohanverma031@gmail.com>
2024-03-13 16:37:36 +0530

    asdfgh

--- src/app/(sample_pages)/landscaping-business/page.tsx
+++ src/app/(sample_pages)/landscaping-business/page.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import style from "./page.module.scss";
 import Home_page from "../components/Home_page";
 import { data, review } from "../../(sample_pages)/data/landscaping";
+
 export default function page() {
-    return (
-        <div>
-            <Home_page data={data} reviews={review} />
-        </div>
-    );
+	return (
+		<div>
+			<Home_page data={data} review={review} />
+		</div>
+	);
 }
