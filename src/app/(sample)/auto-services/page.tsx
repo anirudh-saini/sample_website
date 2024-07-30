b8c382709cf2300edc33cda5c8b981b1214b272b
Anirudh Saini <anirudh.saini852@gmail.com>
2024-07-30 13:24:48 +0530

    changes

--- src/app/(sample)/auto-services/page.tsx
+++ src/app/(sample)/auto-services/page.tsx
@@ -35,6 +35,67 @@ export default function page() {
             <div>
                 <Review />
             </div>
+            {/* <div className={style.map}>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3299.458860173218!2d-83.90784872436855!3d34.211300273097386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5f2ef33d693bd%3A0x7c5b4c5d4c2e1fe0!2s4337%20McBrayer%20Rd%20%23400%2C%20Oakwood%2C%20GA%2030566%2C%20USA!5e0!3m2!1sen!2sin!4v1713159678003!5m2!1sen!2sin"
+                    // width="1920"
+                    // height="348"
+                    style={{ border: 0 }}
+                    allowFullScreen={false}
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                ></iframe> */}
+            {/* 
+                <div className={style.map_content}>
+                    <div>
+                        <h5>Contact Info</h5>
+                        <div className={style.contact}>
+                            <div>
+                                <img src="/location.png" alt="location" />
+                                <p>
+                                    2605 Caton Hill Road, Woodbridge, VA 22192
+                                </p>
+                            </div>
+                            <div>
+                                <img src="/call.png" alt="number" />
+                                <p>1-800-1234567</p>
+                            </div>
+                            <div>
+                                <img src="/email.png" alt="email" />
+                                <p>officeone@youremail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h5>Opening Hours</h5>
+                        <div className={style.time}>
+                            <img src="/time.png" alt="timing" />
+
+                            <div>
+                                <p>
+                                    Mon-Fri: <span>7:00 AM - 6:00 PM</span>
+                                </p>
+                                <p>
+                                    Saturday: <span>9:00 AM - 5:00 PM</span>
+                                </p>
+                                <p>
+                                    Sunday: <span>Closed</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={style.copy}>
+                <div>
+                    <p>© 2022 Car Repair Services, All Rights Reserved</p>
+                    <div>
+                        <img src="/Frame.svg" alt="" />
+                        <img src="/Frame1.svg" alt="" />
+                        <img src="/Frame2.svg" alt="" />
+                    </div>
+                </div>
+            </div> */}
         </div>
     );
 }
